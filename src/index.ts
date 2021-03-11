883fa0ff3827ead8fd172044e52ff479a85f0c7d
Juan Feris <juan.feris@gorillalogic.com>
2021-03-11 15:00:40 -0500

    Created Basic Class for Experimentation with Event Registration

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('Hi!')
+import { User } from './models/User'
+
+const user = new User({})
+user.on('change', () => {})
+user.on('click', () => {})
+user.on('move', () => {})
+
+console.log(user)
---
+++ src/models/User.ts
@@ -0,0 +1,28 @@
+interface UserProps {
+	name?: string
+	age?: number
+}
+
+type Callback = () => void
+
+export class User {
+	events: { [key: string]: Callback[] } = {}
+
+	constructor(private data: UserProps) {}
+
+	get(propName: string): string | number {
+		return this.data[propName]
+	}
+
+	set(update: UserProps): void {
+		Object.assign(this.data, update)
+	}
+
+	on(eventName: string, cb: Callback): void {
+		const handlers = this.events[eventName] || []
+		handlers.push(cb)
+		this.events[eventName] = handlers
+	}
+
+	trigger(eventName: string): void {}
+}
