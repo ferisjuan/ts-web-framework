280b39c7a3d99f938da6b2fa4ba7f68f094d3a1d
Juan Feris <juan.feris@gorillalogic.com>
2021-03-11 16:50:04 -0500

    Added fetch and save

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,7 @@
 import { User } from './models/User'
 
 const user = new User({})
-user.on('change', () => {
-	console.log('change#1')
-})
-user.on('change', () => {
-	console.log('change#2')
-})
-user.on('move', () => {
-	console.log('move')
-})
 
-user.trigger('move')
+user.set({ name: 'new record', age: 0 })
+
+user.save()
