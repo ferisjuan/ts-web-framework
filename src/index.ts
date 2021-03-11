5d13caa0f45abd3e3b131af1f45d2d8645789ca2
Juan Feris <juan.feris@gorillalogic.com>
2021-03-11 15:06:12 -0500

    Implemented Trigger for Events

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import { User } from './models/User'
 
 const user = new User({})
-user.on('change', () => {})
-user.on('click', () => {})
-user.on('move', () => {})
+user.on('change', () => {
+	console.log('change#1')
+})
+user.on('change', () => {
+	console.log('change#2')
+})
+user.on('move', () => {
+	console.log('move')
+})
 
-console.log(user)
+user.trigger('move')
--- src/models/User.ts
+++ src/models/User.ts
@@ -24,5 +24,13 @@ export class User {
 		this.events[eventName] = handlers
 	}
 
-	trigger(eventName: string): void {}
+	trigger(eventName: string): void {
+		const handlers = this.events[eventName]
+
+		if (!handlers || handlers.length === 0) {
+			return
+		}
+
+		handlers.forEach(callback => callback())
+	}
 }
