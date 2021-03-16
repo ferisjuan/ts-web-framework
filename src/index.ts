4f01f149a0717f7f21d3b6d4a376fd97c4cec00f
Juan Feris <juan.feris@gorillalogic.com>
2021-03-16 14:22:46 -0500

    Making Collection a Generic Type

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import { Collection } from './models/Collection'
+import { User, UserProps } from './models/User'
 
-const collection = new Collection('http://localhost:3000/users')
+const collection = new Collection<User, UserProps>(
+	'http://localhost:3000/users',
+	(json: UserProps): User => User.create(json)
+)
 
 collection.on('change', () => {
-	console.log(collection)
+	console.log(collection.models)
 })
 
 collection.fetch()
